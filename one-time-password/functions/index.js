89c55256bf7c8938a32c321d6ac3b985343c9f4d
Andrey <ramirez.jag00@gmail.com>
2018-12-31 03:23:03 +0800

    service account set-up

--- one-time-password/functions/index.js
+++ one-time-password/functions/index.js
@@ -1,18 +1,20 @@
+const admin = require('firebase-admin');
 const functions = require('firebase-functions');
 const createUser = require('./create_user');
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+const serviceAccount = require('./service-account.json');
 
-// since we made an object property 'helloWorld' for exports, google will assume that we will make a function name using this
-// functions.https.onRequest, this is how to setup google cloud functions
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+// use admin to access our data within our function/s
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://one-time-password-797c6.firebaseio.com"
 });
 
-exports.goodBye = functions.https.onRequest((request, response) => {
- response.send("...goodbye!");
-});
+// sample function
+// // since we made an object property 'helloWorld' for exports, google will assume that we will make a function name using this
+// // functions.https.onRequest, this is how to setup google cloud functions
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
 
 // create a function `createUser` and set it up using the createUser function that has been imported
 exports.createUser = functions.https.onRequest(createUser);
