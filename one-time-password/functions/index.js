d249796ab9dbeb1f6f41b5755c1eacfa56b860d0
Andrey <ramirez.jag00@gmail.com>
2018-12-31 02:22:40 +0800

    created a new dummy function

--- one-time-password/functions/index.js
+++ one-time-password/functions/index.js
@@ -3,6 +3,13 @@ const functions = require('firebase-functions');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+// since we made an object property 'helloWorld' for exports, google will assume that we will make a function name using this
+// functions.https.onRequest, this is how to setup google cloud functions
+exports.helloWorld = functions.https.onRequest((request, response) => {
+ response.send("Hello from Firebase!");
+});
+
+exports.goodBye = functions.https.onRequest((request, response) => {
+ response.send("...goodbye!");
+});
