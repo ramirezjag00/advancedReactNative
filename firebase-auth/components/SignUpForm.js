e886b54392ef0e5f009f9374cf56fa6e12da9167
Andrey <ramirez.jag00@gmail.com>
2019-01-02 04:14:25 +0800

    added placeholder and edited state semicolon

--- firebase-auth/components/SignUpForm.js
+++ firebase-auth/components/SignUpForm.js
@@ -10,13 +10,14 @@ import {
 } from 'react-native-elements';
 import axios from 'axios';
 
+// place this on .env file
 const ROOT_URL = 'https://us-central1-one-time-password-797c6.cloudfunctions.net';
 
 class SignUpForm extends Component {
   // ES2017
   // no need for constructor since we won't be doing anything from there except for initializing a state
   state = {
-    phone: '';
+    phone: '',
   };
 
   // ES2017
@@ -40,6 +41,7 @@ class SignUpForm extends Component {
         <View style={{ marginBottom: 10 }}>
           <FormLabel>Enter Phone Number</FormLabel>
           <FormInput
+            placeholder="639XXXXXXXXX"
             value={phone}
             onChangeText={phone => this.setState({ phone })}
           />
