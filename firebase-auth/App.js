c6ced3e0ca6f1dc217a346e3465d957a2777246c
Andrey <ramirez.jag00@gmail.com>
2019-01-02 04:39:02 +0800

    initialization of firebase

--- firebase-auth/App.js
+++ firebase-auth/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import firebase from 'firebase';
+
 import SignUpForm from './components/SignUpForm';
 import SignInForm from './components/SignInForm';
 
 export default class App extends React.Component {
+  componentDidMount() {
+    // move this to a .env file
+    const config = {
+    apiKey: "AIzaSyAeJsPZZLdaxOYGznT_1gjnR-x7lpscNT8",
+    authDomain: "one-time-password-797c6.firebaseapp.com",
+    databaseURL: "https://one-time-password-797c6.firebaseio.com",
+    projectId: "one-time-password-797c6",
+    storageBucket: "one-time-password-797c6.appspot.com",
+    messagingSenderId: "248733918475"
+  };
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <View style={styles.container}>
