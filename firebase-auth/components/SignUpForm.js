2a7c567cdd72472f2dce1e05b85e3627e03ae561
Andrey <ramirez.jag00@gmail.com>
2019-01-02 03:50:23 +0800

    created form value and set up of handleSubmit helper function

--- firebase-auth/components/SignUpForm.js
+++ firebase-auth/components/SignUpForm.js
@@ -10,14 +10,31 @@ import {
 } from 'react-native-elements';
 
 class SignUpForm extends Component {
+  // ES2017
+  // no need for constructor since we won't be doing anything from there except for initializing a state
+  state = {
+    phone: '';
+  };
+
+  // ES2017
+  // no need to do this.handle.bind(this) since the arrow function handles it
+  handleSubmit = () => {
+
+  }
+
   render() {
+    const { phone } = this.state;
+
     return (
       <View>
         <View style={{ marginBottom: 10 }}>
           <FormLabel>Enter Phone Number</FormLabel>
-          <FormInput />
+          <FormInput
+            value={phone}
+            onChangeText={phone => this.setState({ phone })}
+          />
         </View>
-        <Button title="Submit" />
+        <Button onPress={this.handleSubmit} title="Submit" />
       </View>
     );
   }
