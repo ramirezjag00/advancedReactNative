b403143f3e44eb1c2b3c1ae4e1cce9e74ff8fcbd
Andrey <ramirez.jag00@gmail.com>
2019-01-02 04:24:32 +0800

    changed style of app.js

--- firebase-auth/App.js
+++ firebase-auth/App.js
@@ -18,7 +18,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    // alignItems: 'center',
-    // justifyContent: 'center',
+    alignItems: 'center',
+    justifyContent: 'space-around',
   },
 });
