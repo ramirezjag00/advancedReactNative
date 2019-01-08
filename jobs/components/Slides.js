81572866978e37e7d87e98bf4c4dc7445319972b
Andrey <ramirez.jag00@gmail.com>
2019-01-08 16:21:51 +0800

    Edited sample welcomescreen with scroll view

--- jobs/components/Slides.js
+++ jobs/components/Slides.js
@@ -1,19 +1,45 @@
 import React, { Component } from 'react';
 import {
   View,
-  Text
+  Text,
+  ScrollView,
 } from 'react-native';
 
 class Slides extends Component {
+  renderSlides() {
+    const { data } = this.props;
+    return data.map(slide => {
+      return (
+        <View key={slide.text} style={styles.slideContainer}>
+          <Text style={styles.slideText}>{slide.text}</Text>
+        </View>
+      );
+    });
+  }
+
   render() {
     return (
-      <View>
-        <Text>
-          Slides
-        </Text>
-      </View>
+      <ScrollView
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator
+        style={{ flex: 1 }}
+      >
+        {this.renderSlides()}
+      </ScrollView>
     );
   }
+}
+
+const styles = {
+  slideText: {
+    fontSize: 30,
+  },
+  slideContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 }
 
 export default Slides;
