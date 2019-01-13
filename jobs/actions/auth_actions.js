376781c47441439c46cba8763cd256aaee92e3b0
Andrey <ramirez.jag00@gmail.com>
2019-01-13 17:12:11 +0800

    WIP auth_actions

--- jobs/actions/auth_actions.js
+++ jobs/actions/auth_actions.js
@@ -1,13 +1,36 @@
 import { AsyncStorage } from 'react-native';
 import {
-  FACEBOOK_LOGIN_SUCCESS
+  FACEBOOK_LOGIN_SUCCESS,
+  FACEBOOK_LOGIN_FAIL,
 } from './types';
+import { Facebook } from 'expo';
 
 // How to use AsyncStorage:
 // key value store, basically like Local Storage
 // AsyncStorage.setItem('fb_token', token);
 // AsyncStorage.getItem('fb_token');
 
-export const facebookLogin = () => {
 
+// 'async await' syntax with redux-thunk 'dispatch'
+export const facebookLogin = () => async dispatch => {
+  let token = await AsyncStorage.getItem('fb_token');
+  if (token) {
+    // Dispatch an action saying FB login is done
+    dispatch({ type: FACEBOOK_LOGIN_SUCCESS, payload: token });
+  } else {
+    // Start up Fb Login Process
+    handleFacebookLogin();
+  }
+}
+
+cosnt handleFacebookLogin = async () => {
+  let { type, token } = await Facebook.logInWithReadPermissionsAsync('290094984983909', {
+    permissions: ['public_profile']
+  });
+
+  if (type === 'cancel') {
+    return dispatch({ type: 'FACEBOOK_LOGIN_FAIL' });
+  } else {
+
+  }
 };
