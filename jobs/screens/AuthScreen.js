7c7b8fc100eb9b923a5f112198b35913fac29af2
Andrey <ramirez.jag00@gmail.com>
2019-01-14 18:51:18 +0800

    Tested removal of token in storage

--- jobs/screens/AuthScreen.js
+++ jobs/screens/AuthScreen.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {
   View,
   Text,
+  AsyncStorage
 } from 'react-native';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
@@ -9,6 +10,7 @@ import * as actions from '../actions';
 class AuthScreen extends Component {
   componentDidMount() {
     this.props.facebookLogin();
+    AsyncStorage.removeItem('fb_token');
   }
 
   render() {
