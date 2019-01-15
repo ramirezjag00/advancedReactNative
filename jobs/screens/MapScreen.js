310afea60132bb9d8439b315a106e2098e4118e4
Andrey <ramirez.jag00@gmail.com>
2019-01-15 17:18:13 +0800

    Added check to load center of map's region otherwise show loading

--- jobs/screens/MapScreen.js
+++ jobs/screens/MapScreen.js
@@ -2,26 +2,41 @@ import React, { Component } from 'react';
 import {
   View,
   Text,
+  ActivityIndicator,
 } from 'react-native';
 import { MapView } from 'expo';
 
 class MapScreen extends Component {
   state = {
+    mapLoaded: false,
     region: {
       // Makati long, lat from www.google.com.ph
       longitude: 121.0244,
       latitude: 14.5547,
+      //xxxDelta indicates the zoom state of the long, lat
       longitudeDelta: 0.04,
       latitudeDelta: 0.09,
     }
   }
 
+  componentDidMount() {
+    this.setState({ mapLoaded: true });
+  }
+
   render() {
+    const { region, mapLoaded } = this.state;
+    if (!mapLoaded) {
+      return (
+        <View style={{ flex: 1, justifyContent: 'center' }}>
+          <ActivityIndicator size='large' />
+        </View>
+      );
+    }
     return (
       <View style={{ flex: 1 }}>
         <MapView
           style={{ flex: 1 }}
-          initialRegion= {this.state.region}
+          initialRegion= {region}
         />
       </View>
     );
