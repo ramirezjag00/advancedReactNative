35a900ebd02fe86c2ebd2574bd296508ffe75547
Andrey <ramirez.jag00@gmail.com>
2019-01-15 17:11:29 +0800

    Added initial state for region property of MapView

--- jobs/screens/MapScreen.js
+++ jobs/screens/MapScreen.js
@@ -6,10 +6,23 @@ import {
 import { MapView } from 'expo';
 
 class MapScreen extends Component {
+  state = {
+    region: {
+      // Makati long, lat from www.google.com.ph
+      longitude: 121.0244,
+      latitude: 14.5547,
+      longitudeDelta: 0.04,
+      latitudeDelta: 0.09,
+    }
+  }
+
   render() {
     return (
       <View style={{ flex: 1 }}>
-        <MapView style={{ flex: 1 }} />
+        <MapView
+          style={{ flex: 1 }}
+          initialRegion= {this.state.region}
+        />
       </View>
     );
   }
