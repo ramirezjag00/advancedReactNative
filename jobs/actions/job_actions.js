c99ba42600d46cc9336ed76ecde3766b45414cfa
Andrey <ramirez.jag00@gmail.com>
2019-01-18 15:14:00 +0800

    changed typo error

--- jobs/actions/job_actions.js
+++ jobs/actions/job_actions.js
@@ -8,7 +8,7 @@ import qs from 'qs';
 
 const JOB_ROOT_URL = 'http://api.indeed.com/ads/apisearch?';
 
-const JOB_QUERY_PARAMS: {
+const JOB_QUERY_PARAMS =  {
   publisher: '4201738803816157',
   format: 'json',
   v: '2',
