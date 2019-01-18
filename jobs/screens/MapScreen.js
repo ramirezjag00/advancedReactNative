0a927e75112460ee4a959a0dbca1c8f59f4a0c0b
Andrey <ramirez.jag00@gmail.com>
2019-01-18 15:47:01 +0800

    Added button for fetch_jobs action creator

--- jobs/screens/MapScreen.js
+++ jobs/screens/MapScreen.js
@@ -4,7 +4,11 @@ import {
   Text,
   ActivityIndicator,
 } from 'react-native';
+import { Button } from 'react-native-elements';
 import { MapView } from 'expo';
+import { connect } from 'react-redux';
+
+import * as actions from '../actions';
 
 class MapScreen extends Component {
   state = {
@@ -26,6 +30,10 @@ class MapScreen extends Component {
     this.setState({ region });
   }
 
+  onButtonPress = () => {
+    this.props.fetchJobs(this.state.region);
+  }
+
   render() {
     const { region, mapLoaded } = this.state;
     if (!mapLoaded) {
@@ -42,9 +50,27 @@ class MapScreen extends Component {
           initialRegion= {region}
           onRegionChangeComplete={this.onRegionChangeComplete}
         />
+        <View style={styles.buttonContainer}>
+          <Button
+            large
+            title='Search this area'
+            backgroundColor='#009688'
+            icon={{ name: 'search' }}
+            onPress={this.onButtonPress}
+          />
+        </View>
       </View>
     );
   }
 }
 
-export default MapScreen;
+const styles = {
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 20,
+    left: 0,
+    right: 0,
+  }
+}
+
+export default connect(null, actions)(MapScreen);
