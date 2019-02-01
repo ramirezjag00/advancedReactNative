76cdd47eedec982e226411b5bbc885aae62fd2b1
Andrey <ramirez.jag00@gmail.com>
2019-02-01 14:11:33 +0800

    Set up Deckscreen with data

--- jobs/screens/DeckScreen.js
+++ jobs/screens/DeckScreen.js
@@ -1,23 +1,41 @@
 import React, { Component } from 'react';
-import {
-  View,
-  Text,
-} from 'react-native';
+import { View, Text } from 'react-native';
 import { connect } from 'react-redux';
-
+import { MapView } from 'expo';
+import { Card, Button } from 'react-native-elements';
 import Swipe from '../components/Swipe';
-
 class DeckScreen extends Component {
+  renderCard(job) {
+    return (
+      <Card title={job.jobTitle}>
+        <View style={styles.detailWrapper}>
+          <Text>{job.company}</Text>
+          <Text>{job.formattedRelativeTime}</Text>
+        </View>
+        <Text>{job.snippet}</Text>
+      </Card>
+    );
+  }
+
   render() {
     return (
       <View>
         <Text>{this.props.jobs[0].jobtitle}</Text>
         <Swipe
           data={this.props.jobs}
+          renderCard={this.renderCard}
         />
       </View>
     );
   }
+}
+
+const styles = {
+  detailWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginBottom: 10,
+  },
 }
 
 // destructuring state.jobs from combineReducers
